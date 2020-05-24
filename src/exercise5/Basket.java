766014815cd33e6cb7f48b9e739cc663fcff352e
Guilherme Tavares Shimamoto <guilherme_shima@hotmail.com>
2020-05-24 18:25:59 -0300

    Exercício 5

--- src/exercise5/Basket.java
+++ src/exercise5/Basket.java
@@ -2,11 +2,7 @@ package exercise5;
 
 public class Basket {
 
-	// ..
 	float getTotalPrice(Item i) {
-		float price = i.getPrice() + i.getTax();
-		if (i.isOnSale())
-			price = price - i.getSaleDiscount() * price;
-		return price;
+		return i.getCalculatedPrice();
 	}
 }
--- src/exercise5/Item.java
+++ src/exercise5/Item.java
@@ -2,25 +2,37 @@ package exercise5;
 
 public class Item {
 
-	public int getPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+	private float price;
+	private float tax;
+	private float saleDiscount;
+	private boolean isOnSale;
+
+	public Item(float price, float tax, float saleDiscount, boolean isOnSale) {
+		this.price = price;
+		this.tax = tax;
+		this.isOnSale = isOnSale;
+		this.saleDiscount = saleDiscount;
 	}
 
-	public int getTax() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setOnSale(boolean onSale, float saleDiscount) {
+		this.isOnSale = onSale;
+		this.saleDiscount = saleDiscount;
 	}
 
-	public boolean isOnSale() {
-		// TODO Auto-generated method stub
-		return false;
+	private float getPriceWithTax() {
+		return price + tax;
 	}
 
-	public float getSaleDiscount() {
-		// TODO Auto-generated method stub
-		return 0;
+	private float getPriceOnSale() {
+		float priceWithTax = getPriceWithTax();
+		float discount = priceWithTax * saleDiscount;
+		return priceWithTax - discount;
 	}
 
-	//...
+	public float getCalculatedPrice() {
+		if(isOnSale) {
+			return getPriceOnSale();
+		}
+		return getPriceWithTax();
+	}
 }
