ced12e4546a8ffbdfa8e7984f773e55d74896db2
pdmau <maurilio032@gmail.com>
2020-05-24 19:48:24 -0300

    Exercicio 4

--- src/exercise4/Course.java
+++ src/exercise4/Course.java
@@ -1,12 +1,15 @@
 package exercise4;
 
 public class Course {
+	private String name;
+	private boolean isAdvanced;
+	
 	public Course(String name, boolean isAdvanced) {
-		// ...
+		this.name = name;
+		this.isAdvanced = isAdvanced;
 	}
 
 	public boolean isAdvanced() {
-		// ...
-		return false;
+		return this.isAdvanced;
 	}
 }
